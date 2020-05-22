93546f53841d06e76675f8402e1b8def0e8f9987
grundel <daniel@grundel.me>
2020-05-22 16:17:39 -0700

    add time string

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ var ifaces = os.networkInterfaces();
 const fontFamily = 'noto';
 const fontSize = 16;
 
-const ipAddrs = Object.keys(ifaces).reduce((ips, ifname) => {
+const getIpAddresses = () => Object.keys(ifaces).reduce((ips, ifname) => {
     return ips.concat(ifaces[ifname]
         .filter(iface => iface.family === 'IPv4' && iface.internal === false)
         .map(iface => iface.address));
 }, []);
 
-// Returns a framebuffer in direct mode
-var fb = pitft('/dev/fb1'); 
+const getDateString = () => {
+    const now = new Date();
+    return now.toISOString();
+}
+
+// Returns a framebuffer in double buffering mode
+var fb = pitft("/dev/fb1", true);
 
 // Clear the screen buffer
 fb.clear();
@@ -23,5 +28,15 @@ var yMax = fb.size().height;
 fb.color(1, 1, 1); // Set the color to white
 
 fb.font(fontFamily, fontSize);
-// Draw the text non-centered, non-rotated, left
-fb.text(0, 20, ipAddrs.join(', '), false, 0);
+
+var update = function() {
+    // Draw the text non-centered, non-rotated, left
+    fb.text(0, 20, getIpAddresses().join(', '), false, 0);
+    fb.text(20, 20, getDateString(), false, 0);
+
+    fb.blit(); // Transfer the back buffer to the screen buffer
+};
+
+setInterval(function() {
+    update();
+}, 100);
