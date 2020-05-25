21d185839183082c0af581fe456f2ee70858be39
grundel <daniel@grundel.me>
2020-05-25 16:57:21 -0700

    add more data points for cpu stats

--- src/modules/stats/cpuStats.ts
+++ src/modules/stats/cpuStats.ts
@@ -3,4 +3,4 @@ import { StatData } from '../StatData';
 
 export type CpuLoad = [number, number, number];
 
-export const cpuStats = new StatData<CpuLoad>(10, 60000, () => (os.loadavg() as CpuLoad));
+export const cpuStats = new StatData<CpuLoad>(40, 15000, () => (os.loadavg() as CpuLoad));
